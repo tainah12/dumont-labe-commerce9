b58da2e148117af5c95fe3f2f1029bda47a46f01
rafae <rafaelgf@gmail.com>
2020-10-25 22:06:46 -0300

    Carrinho funcional

--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 250px;
-  height: 90vh;
+  height: 88vh;
   display: flex;
   flex-direction: column;
   align-items: center;
